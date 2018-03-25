2c4ee52b71c9b60ad6cd3660dd77e95ae2d682c4
Mathilde Longchamp <mathilde.longchamp@gmail.com>
2018-03-25 22:43:19 +0200

    second phase

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var clear	     = document.getElementById('clear');
 var reset	     = document.getElementById('reset');
+var error  		 = document.getElementById('error');
 var submit	     = document.getElementById('submit');
 var result	     = document.getElementById('result');
 var userNumber   = document.querySelector("input");
@@ -7,6 +8,8 @@ var printNumber  = document.getElementById('number');
 var RandomNumber = Math.floor(Math.random() * 101);
 
 console.log(RandomNumber)
+
+
 
 var checkValue = function(){
 	printNumber.innerHTML = userNumber.value
@@ -21,24 +24,60 @@ var checkValue = function(){
 	else if (number < RandomNumber){
 		result.innerHTML = "That is too low"
 	}
-	userNumber.value = null;
 };
 
-submit.onclick = checkValue;
-userNumber.onkeypress = function(e){
-    if (e.keyCode == 13){
-        checkValue()
+var ValueBetween0To100 = function(){
+	var number = parseInt(userNumber.value, 10);
+	console.log(number)
+	if(number > 100){
+		printNumber.innerHTML = 100;
+		error.innerHTML = "100 is the max value"
+
+	}
+	else if (number < 0){
+		printNumber.innerHTML = 0;
+		error.innerHTML = "0 is the min value"
 	}
 };
 
+
+submit.onclick = function(){
+	checkValue()
+	ValueBetween0To100()
+	userNumber.value = "";
+}
+
+userNumber.onkeypress = function(e){
+    if (e.keyCode == 13){
+        checkValue()
+        ValueBetween0To100()
+        userNumber.value = "";
+	}
+};
+
+userNumber.onkeyup = function(e){
+	disable();
+};
+
 clear.onclick = function(){
-	userNumber.value = null;
+	userNumber.value = "";
 }
 
 reset.onclick = function(){
-	userNumber.value = null;
-	printNumber.innerHTML = null;
+	userNumber.value = "";
+	printNumber.innerHTML = "";
 	result.innerHTML = ""
+	error.innerHTML = ""
 	var RandomNumber = Math.floor(Math.random() * 101);
 	console.log(RandomNumber)
+}
+
+var disable = function(){
+	var disable = userNumber.value === "";
+	clear.disabled = disable;
+	reset.disabled = disable;
+	submit.disabled = disable;
+	clear.className = 'disable'
+	clear.className = 'disable'
+	submit.className = 'disable'
 }
